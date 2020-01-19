5d2ad47f18b0341a01e26f013a6142f0db9f523e
Michael Bass <mgbass11@gmail.com>
2020-01-18 20:09:33 -0500

    all scenes sequences quest

---
+++ Assets/ClassRoomAssets/DebugScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestFunction(); 
+    }
+
+
+    void TestFunction() {
+    	Debug.Log("Script Fired Test"); 
+    }
+
+     public void Hit() {
+        Debug.Log("ITEM WAS HIT! "); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
